ccae203fdba3c5fcde2388c837bd719bbe9611ba
Hans ONG <93074650+ongyouyang@users.noreply.github.com>
2023-09-16 17:17:55 +0800

    cleanup-audio
    cleanup the audio

--- Purring_Engine/src/AudioManager.cpp
+++ Purring_Engine/src/AudioManager.cpp
@@ -1,7 +1,31 @@
+/*!***********************************************************************************
+ \project  Purring Engine
+ \module   CSD2401-A
+ \file     AudioManager.cpp
+ \date     16-09-2023
+
+ \author               You Yang ONG
+ \par      email:      youyang.o@digipen.edu
+
+ \brief    This file contains the RendererManager class, which manages
+           the render passes and includes helper functions to draw debug shapes.
+
+ All content (c) 2023 DigiPen Institute of Technology Singapore. All rights reserved.
+*************************************************************************************/
+
 #include "prpch.h"
 #include "AudioManager.h"
 #include <iostream>
+/*                                                                                                          includes
+--------------------------------------------------------------------------------------------------------------------- */
 
+/*-----------------------------------------------------------------------------
+/// <summary>
+/// Default constructor for the AudioManager class.
+/// Initializes member pointers to nullptr, ensuring they are in a safe state.
+/// FMOD system, sound, and channel resources will be allocated later in Init().
+/// </summary>
+----------------------------------------------------------------------------- */
 AudioManager::AudioManager()
     : system(nullptr),
     sound1(nullptr),
@@ -11,6 +35,15 @@ AudioManager::AudioManager()
 {
 }
 
+
+
+/*-----------------------------------------------------------------------------
+/// <summary>
+/// Default constructor for the AudioManager class.
+/// Initializes member pointers to nullptr, ensuring they are in a safe state.
+/// FMOD system, sound, and channel resources will be allocated later in Init().
+/// </summary>
+----------------------------------------------------------------------------- */
 AudioManager::~AudioManager()
 {
     if (sound1) sound1->release();
@@ -18,40 +51,87 @@ AudioManager::~AudioManager()
     if (system) system->release();
 }
 
+
+
+/*-----------------------------------------------------------------------------
+/// <summary>
+/// Initializes the FMOD audio system and sound resources.
+/// This method will create the FMOD System object, initialize it,
+/// and also create Sound objects for sound1 and sound2.
+/// </summary>
+/// <returns>
+/// Returns true if all initializations are successful, otherwise returns false.
+/// </returns>
+----------------------------------------------------------------------------- */
 bool AudioManager::Init()
 {
+    // Create the FMOD System object
     FMOD_RESULT result = FMOD::System_Create(&system);
-    if (result != FMOD_OK) {
+
+    // Check if FMOD System object was successfully created
+    if (result != FMOD_OK)
+    {
         std::cout << "FMOD System_Create failed: " << FMOD_ErrorString(result) << "\n";
-        return false;
+        return false;                           // Return false if init failed
     }
 
+    // Initialize the FMOD System object with 512 channels
     result = system->init(512, FMOD_INIT_NORMAL, 0);
-    if (result != FMOD_OK) {
+
+    // Check if FMOD System was successfully initialized
+    if (result != FMOD_OK)
+    {
         std::cout << "FMOD init failed: " << FMOD_ErrorString(result) << "\n";
-        return false;
+        return false;                           // Return false if init failed
     }
 
+    // Create the sound object for sound1
     result = system->createSound("Audio/sound1.wav", FMOD_DEFAULT, 0, &sound1);
-    if (result != FMOD_OK) {
+
+    // Check if sound1 was successfully created
+    if (result != FMOD_OK)
+    {
         std::cout << "FMOD createSound failed for sound1: " << FMOD_ErrorString(result) << "\n";
-        return false;
+        return false;                           // Return false if init failed
     }
 
+    // Create the sound object for sound2
     result = system->createSound("Audio/sound2.wav", FMOD_DEFAULT, 0, &sound2);
-    if (result != FMOD_OK) {
+
+    // Check if sound2 was successfully created
+    if (result != FMOD_OK)
+    {
         std::cout << "FMOD createSound failed for sound2: " << FMOD_ErrorString(result) << "\n";
-        return false;
+        return false;                           // Return false if init failed
     }
 
     return true;
 }
 
+
+
+/*-----------------------------------------------------------------------------
+/// <summary>
+/// Updates the FMOD audio system.
+/// This method calles once per frame keeping the FMOD system updated.
+/// </summary>
+----------------------------------------------------------------------------- */
 void AudioManager::Update()
 {
     system->update();
 }
 
+
+
+/*-----------------------------------------------------------------------------
+/// <summary>
+/// Plays a sound specified by the file path.
+/// The method maps the given filePath to a preloaded FMOD::Sound object and plays it.
+/// </summary>
+/// <param name="filePath">
+/// The file path of the sound to be played.
+/// </param>
+----------------------------------------------------------------------------- */
 void AudioManager::PlaySound(const char* filePath)
 {
     FMOD::Sound* sound = nullptr;
@@ -74,6 +154,14 @@ void AudioManager::PlaySound(const char* filePath)
     }
 }
 
+
+
+/*-----------------------------------------------------------------------------
+/// <summary>
+/// Stops any sound currently being played on channel1 and channel2.
+/// This method checks if the channels are initialized and then stops the sound playback.
+/// </summary>
+----------------------------------------------------------------------------- */
 void AudioManager::StopSound()
 {
     if (channel1) channel1->stop();
--- Purring_Engine/src/AudioManager.h
+++ Purring_Engine/src/AudioManager.h
@@ -1,9 +1,24 @@
+/*!***********************************************************************************
+ \project  Purring Engine
+ \module   CSD2401-A
+ \file     AudioManager.h
+ \date     16-09-2023
+
+ \author               You Yang ONG
+ \par      email:      youyang.o@digipen.edu
+
+ \brief    This file contains the AudioManager class, which manages
+           audio playback and includes FMOD as the underlying audio system.
+
+ All content (c) 2023 DigiPen Institute of Technology Singapore. All rights reserved.
+*************************************************************************************/
 #pragma once
 #include "fmod.hpp"
 #include "fmod_errors.h"
 class AudioManager
 {
 public:
+    // Constructor - Destructor
     AudioManager();
     ~AudioManager();
 
@@ -13,9 +28,14 @@ public:
     void StopSound();
 
 private:
+    // FMOD system object for managing audio resources
     FMOD::System* system;
+
+    // FMOD sound objects to hold audio data
     FMOD::Sound* sound1;
     FMOD::Sound* sound2;
+
+    // FMOD channel objects to control playback
     FMOD::Channel* channel1;
     FMOD::Channel* channel2;
 };
