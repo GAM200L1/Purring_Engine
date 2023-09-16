b342850b774a55f67a14d8c5693fb3d5392e6eeb
Hans ONG <thehansong@gmail.com>
2023-09-17 00:08:56 +0800

    Merge Main into Audio (#66)
    Co-authored-by: Hans ONG <93074650+ongyouyang@users.noreply.github.com>
    Co-authored-by: Jatyz <110804341+Jatyz@users.noreply.github.com>

--- Purring_Engine/src/Imgui/ImGuiWindow.h
+++ Purring_Engine/src/Imgui/ImGuiWindow.h
@@ -1,19 +1,3 @@
-/*!***********************************************************************************
-
- \project  Purring Engine
- \module   CSD2401-A
- \file     ImGuiWindow.h
- \date     9/5/2023
-
- \author               Jarran Tan Yan Zhi
- \par      email:      jarranyanzhi.tan@digipen.edu
-
- \brief
-Header file containing the declaration and definition of the event and event dispatcher template
-
- All content (c) 2023 DigiPen Institute of Technology Singapore. All rights reserved.
-
-*************************************************************************************/
 #pragma once
 
 #include "../Events/EventHandler.h"
@@ -25,6 +9,7 @@ Header file containing the declaration and definition of the event and event dis
 #include <backends/imgui_impl_glfw.h>
 #include <backends/imgui_impl_opengl3.h>
 #include <misc/cpp/imgui_stdlib.h>
+#include <iostream>
 
 #include "Graphics/GLHeaders.h"
 
@@ -158,3 +143,4 @@ namespace PE {
 		float m_renderWindowWidth, m_renderWindowHeight;
 	};
 }
+
