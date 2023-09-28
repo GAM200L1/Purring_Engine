bac014662ad91c47e045e476017e53a681aac368
l-yn3 <liew.yn0311@gmail.com>
2023-09-28 23:08:29 +0800

    Added Movement as to triggered

--- Purring_Engine/src/WindowManager.cpp
+++ Purring_Engine/src/WindowManager.cpp
@@ -179,6 +179,23 @@ namespace PE
 				}
 			}
 
+			// Movement
+			if (ev.keycode == GLFW_KEY_W)
+			{
+				EntityManager::GetInstance().Get<RigidBody>(1).ApplyForce(vec2{ 0.f,1.f } *5000.f);
+			}
+			if (ev.keycode == GLFW_KEY_A)
+			{
+				EntityManager::GetInstance().Get<RigidBody>(1).ApplyForce(vec2{ -1.f,0.f }*5000.f);
+			}
+			if (ev.keycode == GLFW_KEY_S)
+			{
+				EntityManager::GetInstance().Get<RigidBody>(1).ApplyForce(vec2{ 0.f,-1.f }*5000.f);
+			}
+			if (ev.keycode == GLFW_KEY_D)
+			{
+				EntityManager::GetInstance().Get<RigidBody>(1).ApplyForce(vec2{ 1.f,0.f }*5000.f);
+			}
 			// dash
 			if (EntityManager::GetInstance().GetComponentPool<RigidBody>().HasEntity(1))
 			{
