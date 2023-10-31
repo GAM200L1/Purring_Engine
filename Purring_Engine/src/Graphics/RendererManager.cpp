2233fcc56599416983ebe0f90ad314b8e2e48bd4
krystal.y <krystal.y@digipen.edu>
2023-10-31 19:43:55 +0800

    Add debug calls

--- Purring_Engine/src/Graphics/RendererManager.cpp
+++ Purring_Engine/src/Graphics/RendererManager.cpp
@@ -2,10 +2,9 @@
  \project  Purring Engine
  \module   CSD2401-A
  \file     RendererManager.cpp
- \date:       30-08-2023
+ \date:    30-08-2023
 
  \author:              Krystal YAMIN
-
  \par      email:      krystal.y@digipen.edu
 
  \brief    This file contains the RendererManager class, which manages 
@@ -48,6 +47,53 @@ namespace PE
 {
     namespace Graphics
     {
+        void APIENTRY glDebugOutput(GLenum source,
+            GLenum type,
+            unsigned int id,
+            GLenum severity,
+            GLsizei length,
+            const char* message,
+            const void* userParam)
+        {
+            // ignore non-significant error/warning codes
+            if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
+
+            std::cout << "---------------" << std::endl;
+            std::cout << "Debug message (" << id << "): " << message << std::endl;
+
+            switch (source)
+            {
+            case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+            case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
+            case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
+            case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
+            case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
+            case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+            } std::cout << std::endl;
+
+            switch (type)
+            {
+            case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+            case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+            case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
+            case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+            case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+            case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+            case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+            case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+            case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+            } std::cout << std::endl;
+
+            switch (severity)
+            {
+            case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
+            case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
+            case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
+            case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+            } std::cout << std::endl;
+            std::cout << std::endl;
+        }
+
         RendererManager::RendererManager(GLFWwindow* p_window, CameraManager& r_cameraManagerArg)
             : p_glfwWindow{ p_window }, r_cameraManager{ r_cameraManagerArg }
         {
@@ -60,9 +106,15 @@ namespace PE
                 throw;
             }
 
+            glEnable(GL_DEBUG_OUTPUT);
+            glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+            glDebugMessageCallback(glDebugOutput, nullptr);
+            glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+
             int width, height;
             glfwGetWindowSize(p_glfwWindow, &width, &height);
             m_imguiFrameBuffer.CreateFrameBuffer(width, height);
+
 
             Editor::GetInstance().Init(p_window);
         }
