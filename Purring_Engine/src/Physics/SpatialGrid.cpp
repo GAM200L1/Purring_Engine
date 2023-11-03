42c53dcd8bbb70aadae447041b1fe409299f7236
Foong Jun Wei <f.junwei@digipen.edu>
2023-11-03 15:21:09 +0800

    Fix merge errors

--- Purring_Engine/src/Physics/SpatialGrid.cpp
+++ Purring_Engine/src/Physics/SpatialGrid.cpp
@@ -254,9 +254,5 @@ namespace PE
 		return m_gridSize;
 	}
 
-	vec2 const& Grid::GetGridSize() const { return m_gridSize; }
-	vec2 const& Grid::GetGridMin() const { return m_min; }
-	vec2 const& Grid::GetGridMax() const { return m_max; }
-	float Grid::GetCellWitdh() const { return m_cellWidth; }
-	vec2 Grid::GetColumnsRows() const { return vec2{ m_columns, m_rows }; }
+	
 }
