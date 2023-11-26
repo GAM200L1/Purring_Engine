103c5c8b582a5e8d3c4662ab04c372163cfa756f
Foong Jun Wei <f.junwei@digipen.edu>
2023-11-26 21:21:22 +0800

    Fix GameRelease exit code & GameRelease collision not working

--- Purring_Engine/src/Physics/CollisionManager.cpp
+++ Purring_Engine/src/Physics/CollisionManager.cpp
@@ -113,13 +113,13 @@ namespace PE
 			if (m_grid.GridExists())
 				m_grid.ClearGrid();
 		}
+#endif
 
 		if (m_grid.GetGridSize() != gridSize)
 		{
 			// sets up the grid if it did not exist during runtime
 			m_grid.SetupGrid(gridSize.x, gridSize.y);
 		}
-#endif
 		
 		// updates the grid during runtime
 #ifndef GAMERELEASE
