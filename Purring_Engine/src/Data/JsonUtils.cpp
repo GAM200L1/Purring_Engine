6ee5cb8dc84829880e8d08acafacced885fa14e2
Hans ONG <93074650+ongyouyang@users.noreply.github.com>
2023-11-27 18:12:24 +0800

    Documentation JsonUtils.cpp and h

--- Purring_Engine/src/Data/JsonUtils.cpp
+++ Purring_Engine/src/Data/JsonUtils.cpp
@@ -1,3 +1,19 @@
+/*!***********************************************************************************
+ \project  Purring Engine
+ \module   CSD2401-A
+ \file     JsonUtils.cpp
+ \date     25-10-2023
+
+ \author               Hans (You Yang) ONG
+ \par      email:      youyang.o@digipen.edu
+
+ \brief	   This file contains the implementation of JsonUtils, a utility class in the 
+           Purring Engine, designed for serializing and deserializing various data 
+           types into/from JSON format. The class supports basic data types like int, 
+           float, and string, as well as more complex types like vectors of integers.
+
+ All content (c) 2023 DigiPen Institute of Technology Singapore. All rights reserved.
+*************************************************************************************/
 #include "prpch.h"
 #include "JsonUtils.h"
 #include <iostream>
