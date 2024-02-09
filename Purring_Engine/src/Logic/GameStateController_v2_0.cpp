567bfeea859bb124a74d3c4daccf586813f0eabd
Jtyz <jjjjtyz@gmail.com>
2024-02-09 23:01:52 +0800

    works now

--- Purring_Engine/src/Logic/GameStateController_v2_0.cpp
+++ Purring_Engine/src/Logic/GameStateController_v2_0.cpp
@@ -1250,14 +1250,14 @@ namespace PE
 		for (auto [RatID, RatType] : RatManager->GetRats(RatManager->mainInstance))
 		{
 			//Finished = GETSCRIPTINSTANCEPOINTER(RatScript_v2_0)->GetScriptData()[RatID].finishedExecution;
-			Finished = GETSCRIPTINSTANCEPOINTER(RatScript)->GetScriptData()[RatID].finishedExecution;
+			Finished = Finished && GETSCRIPTINSTANCEPOINTER(RatScript)->GetScriptData()[RatID].finishedExecution;
 			std::cout << "GameStateController_v2_0::CheckFinishExecution() RatID " << RatID << " finished exec " << Finished << " \n";
 		}
 
 		for (auto [CatID, CatType] : CatManager->GetCurrentCats(CatManager->mainInstance))
 		{
 			if(!GETSCRIPTINSTANCEPOINTER(CatScript_v2_0)->GetScriptData()[CatID].isCaged)
-			Finished = GETSCRIPTINSTANCEPOINTER(CatScript_v2_0)->GetScriptData()[CatID].finishedExecution;
+			Finished = Finished && GETSCRIPTINSTANCEPOINTER(CatScript_v2_0)->GetScriptData()[CatID].finishedExecution;
 			std::cout << "GameStateController_v2_0::CheckFinishExecution() CatID " << CatID << " finished exec " << Finished << " \n";
 		}
 
