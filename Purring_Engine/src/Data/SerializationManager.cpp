41978dd9006270e2981699750ab792c24f6bd80e
Foong Jun Wei <f.junwei@digipen.edu>
2024-03-10 17:41:03 +0800

    Silenced c4702

--- Purring_Engine/src/Data/SerializationManager.cpp
+++ Purring_Engine/src/Data/SerializationManager.cpp
@@ -550,7 +550,7 @@ nlohmann::json SerializationManager::LoadMetaDataFromFile(const std::filesystem:
     return loadedData;
 }
 
-
+#pragma warning (disable : 4702)
 size_t SerializationManager::LoadPrefabFromFile(nlohmann::json& r_json)
 {
     if (r_json.contains("Prefab")) // following multi prefab method
@@ -596,7 +596,7 @@ size_t SerializationManager::LoadPrefabFromFile(nlohmann::json& r_json)
     }
     return MAXSIZE_T;
 }
-
+#pragma warning (default : 4702)
 void SerializationManager::LoadLoaders()
 {
     m_initializeComponent.emplace("Transform", &SerializationManager::LoadTransform);
