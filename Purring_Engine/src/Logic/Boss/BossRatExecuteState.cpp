7e7fb9474e2565caafbf5157752f2dddf2527f0c
Jtyz <jjjjtyz@gmail.com>
2024-03-10 16:14:01 +0800

    changes to color
    changed puddle radius check

--- Purring_Engine/src/Logic/Boss/BossRatExecuteState.cpp
+++ Purring_Engine/src/Logic/Boss/BossRatExecuteState.cpp
@@ -66,7 +66,7 @@ namespace PE
 				
 				Transform catTransform = EntityManager::GetInstance().Get<Transform>(CatID);
 				float DistanceFromPuddle = EntityManager::GetInstance().Get<Transform>(id).position.Distance(catTransform.position);
-				if (DistanceFromPuddle + catTransform.width/2 < EntityManager::GetInstance().Get<Transform>(id).width/2)
+				if (DistanceFromPuddle + catTransform.width/2 + 5 < EntityManager::GetInstance().Get<Transform>(id).width/2 - 5)
 				{
 					GETSCRIPTINSTANCEPOINTER(CatController_v2_0)->KillCat(CatID);
 				}
