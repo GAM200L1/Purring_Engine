e713c3fa9e48016471b92aa6be7d5c909ee32e46
Jtyz <Jtyz@users.noreply.github.com>
2024-03-12 10:55:35 +0800

    added more range from wall for deployment

--- Purring_Engine/src/Logic/DeploymentScript.cpp
+++ Purring_Engine/src/Logic/DeploymentScript.cpp
@@ -137,7 +137,7 @@ namespace PE
 		else
 			return;
 
-		if (CheckArea(m_deploymentZone, m_mousepos, cc.radius))
+		if (CheckArea(m_deploymentZone, m_mousepos, cc.radius + 10))
 		{
 			for (auto cid : EntityManager::GetInstance().Get<EntityDescriptor>(m_scriptData[id].NoGoArea).children)
 			{
