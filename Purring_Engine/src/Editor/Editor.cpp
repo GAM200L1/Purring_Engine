b73abf748e0ecbb42d3f3167ce66344b1eada6a8
Foong Jun Wei <f.junwei@digipen.edu>
2024-03-15 11:48:27 +0800

    Bugfix for assorted bugs related to engine (less particles) (#462)
    * Fix display issues and saving issues for prefab editor
    
    * Fix load prefab issues
    
    * fix kagebunshin problems
    
    * potentially evicted ninjas
    
    * unfked the entityID displayed
    
    * play pause stop fix version 2
    
    * load scene reset
    
    ---------
    
    Co-authored-by: Jtyz <Jtyz@users.noreply.github.com>

--- Purring_Engine/src/Editor/Editor.cpp
+++ Purring_Engine/src/Editor/Editor.cpp
@@ -453,11 +453,12 @@ namespace PE {
 					ImGui::Separator();
 					if (ImGui::Selectable("Yes"))
 					{
-						if (EntityManager::GetInstance().Has<EntityDescriptor>(1))
+						size_t tid{ Hierarchy::GetInstance().GetParentOrder().front() };
+						if (EntityManager::GetInstance().Has<EntityDescriptor>(tid))
 						{
-							nlohmann::json save = serializationManager.SerializeEntityPrefab(1);
-							prefabTP = EntityManager::GetInstance().Get<EntityDescriptor>(1).prefabType;
-							prefabCID = EntityManager::GetInstance().GetComponentIDs(1);
+							nlohmann::json save = serializationManager.SerializeEntityPrefab(static_cast<int>(tid));
+							prefabTP = EntityManager::GetInstance().Get<EntityDescriptor>(tid).prefabType;
+							prefabCID = EntityManager::GetInstance().GetComponentIDs(tid);
 							m_applyPrefab = true;
 
 							std::ofstream outFile(prefabFP);
@@ -693,11 +694,21 @@ namespace PE {
 		{
 			ImGui::Indent();
 			std::map<EntityID, EntityID> childOrder;
+			std::vector<EntityID> rm;
 			for (const auto& childID : EntityManager::GetInstance().Get<EntityDescriptor>(r_id).children)
 			{
+				if (!EntityManager::GetInstance().IsEntityValid(childID))
+				{
+					rm.push_back(childID);
+					continue;
+				}
 				while (childOrder.count(EntityManager::GetInstance().Get<EntityDescriptor>(childID).sceneID))
 					++EntityManager::GetInstance().Get<EntityDescriptor>(childID).sceneID;
 				childOrder[EntityManager::GetInstance().Get<EntityDescriptor>(childID).sceneID] = childID;
+			}
+			for (const auto& id : rm)
+			{
+				Hierarchy::GetInstance().DetachChild(id);
 			}
 			for (auto [k,v] : childOrder)
 			{
@@ -1028,7 +1039,7 @@ namespace PE {
 	void Editor::ShowComponentWindow(bool* p_active)
 	{
 		if (IsEditorActive())
-		if (!ImGui::Begin("Property Editor Window", p_active, IsEditorActive() ? 0 : ImGuiWindowFlags_NoInputs) || (m_isPrefabMode && !EntityManager::GetInstance().Has<EntityDescriptor>(1)))
+		if (!ImGui::Begin("Property Editor Window", p_active, IsEditorActive() ? 0 : ImGuiWindowFlags_NoInputs))
 		{
 			ImGui::End();
 		}
@@ -1045,9 +1056,6 @@ namespace PE {
 					for (const ComponentID& name : components)
 					{
 						++componentCount;//increment unique id
-
-						
-
 
 						// ---------- ENTITY DESCRIPTOR ---------- //
 						ImGui::SetNextItemAllowOverlap(); // allow the stacking of buttons
@@ -1151,7 +1159,14 @@ namespace PE {
 									{
 										if (prop.is_readonly())
 										{
-											ImGui::SameLine(); ImGui::Text(std::to_string(vp.get_value<EntityID>()).c_str());
+											if (prop.get_name().to_string() == "Entity ID")
+											{
+												ImGui::SameLine(); ImGui::Text(std::to_string(entityID).c_str());
+											}
+											else
+											{
+												ImGui::SameLine(); ImGui::Text(std::to_string(vp.get_value<EntityID>()).c_str());
+											}
 										}
 									}
 								}
@@ -4451,7 +4466,8 @@ namespace PE {
 									}
 									ClearObjectList();
 									engine_logger.AddLog(false, "Entities Cleared.", __FUNCTION__);
-									ResourceManager::GetInstance().LoadPrefabFromFile(prefabFP, true);
+									m_currentSelectedObject = static_cast<int>(ResourceManager::GetInstance().LoadPrefabFromFile(prefabFP, true));
+									m_objectIsSelected = true;
 								}
 								if (ImGui::Selectable("Edit properties"))
 								{
@@ -5215,17 +5231,25 @@ namespace PE {
 						m_isRunTime = true;
 						m_showEditor = false;
 						m_showGameView = true;
-						engine_logger.AddLog(false, "Attempting to save all entities to file...", __FUNCTION__);
-						SaveAndPlayScene();
+						UndoStack::GetInstance().ClearStack();
 
-						engine_logger.AddLog(false, "Entities saved successfully to file.", __FUNCTION__);
+						if (!m_gameplayPaused)
+						{
+							engine_logger.AddLog(false, "Attempting to save all entities to file...", __FUNCTION__);
+							SaveAndPlayScene();
+							engine_logger.AddLog(false, "Entities saved successfully to file.", __FUNCTION__);
+						}
+						else
+						{
+							m_gameplayPaused = false;
+						}
 					}
 					ImGui::SameLine();
 					ImGui::BeginDisabled();
 					if (ImGui::Button("Stop")) {
 						m_showEditor = true;
 
-						if (m_isRunTime)
+						if (m_isRunTime && !m_gameplayPaused)
 						{
 							StopAndLoadScene();
 
@@ -5258,9 +5282,10 @@ namespace PE {
 					ImGui::SameLine();
 					if (ImGui::Button(" Save "))
 					{
-						if (EntityManager::GetInstance().Has<EntityDescriptor>(1))
+						size_t tid{ Hierarchy::GetInstance().GetParentOrder().front() };
+						if (EntityManager::GetInstance().Has<EntityDescriptor>(tid))
 						{
-							nlohmann::json save = serializationManager.SerializeEntityPrefab(1);
+							nlohmann::json save = serializationManager.SerializeEntityPrefab(static_cast<int>(tid));
 							std::ofstream outFile(prefabFP);
 							if (outFile)
 							{
@@ -5278,11 +5303,12 @@ namespace PE {
 
 						if (ImGui::Selectable("Yes"))
 						{
-							if (EntityManager::GetInstance().Has<EntityDescriptor>(1))
+							size_t tid{ Hierarchy::GetInstance().GetParentOrder().front() };
+							if (EntityManager::GetInstance().Has<EntityDescriptor>(tid))
 							{
-								nlohmann::json save = serializationManager.SerializeEntityPrefab(1);
-								prefabTP = EntityManager::GetInstance().Get<EntityDescriptor>(1).prefabType;
-								prefabCID = EntityManager::GetInstance().GetComponentIDs(1);
+								nlohmann::json save = serializationManager.SerializeEntityPrefab(static_cast<int>(tid));
+								prefabTP = EntityManager::GetInstance().Get<EntityDescriptor>(tid).prefabType;
+								prefabCID = EntityManager::GetInstance().GetComponentIDs(tid);
 								std::ofstream outFile(prefabFP);
 								if (outFile)
 								{
@@ -5380,9 +5406,11 @@ namespace PE {
 								{
 									engine_logger.AddLog(false, "Attempting to save prefab entities to file...", __FUNCTION__);
 									
-									if (EntityManager::GetInstance().Has<EntityDescriptor>(1))
+									size_t tid{ Hierarchy::GetInstance().GetParentOrder().front() };
+									if (EntityManager::GetInstance().Has<EntityDescriptor>(tid))
 									{
-										nlohmann::json save = serializationManager.SerializeEntityPrefab(1);
+										nlohmann::json save = serializationManager.SerializeEntityPrefab(static_cast<int>(tid));
+	
 
 										std::ofstream outFile(prefabFP);
 										if (outFile)
@@ -5431,6 +5459,7 @@ namespace PE {
 										// load scene from filepath
 										SceneManager::GetInstance().LoadSceneToLoad(filePath.substr(filePath.find_last_of('\\') + 1));
 										engine_logger.AddLog(false, "Entities loaded successfully from file.", __FUNCTION__);
+										m_gameplayPaused = false;;
 									}
 									else
 									{
@@ -5971,6 +6000,7 @@ namespace PE {
 		{
 			m_isRunTime = false;
 			toDisable = false;
+			m_gameplayPaused = true;
 		}
 		ImGui::EndDisabled();
 		ImGui::SameLine();
@@ -6271,7 +6301,6 @@ namespace PE {
 		// save active scene name
 		m_savedScene = SceneManager::GetInstance().GetActiveScene();
 		serializationManager.SerializeScene("Savestate/savestate.scene");
-		UndoStack::GetInstance().ClearStack();
 	}
 
 	void Editor::StopAndLoadScene()
