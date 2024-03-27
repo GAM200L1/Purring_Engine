ec13771efeca3eadfb05f45592a91e3321dba926
Hans ONG <thehansong@gmail.com>
2024-03-27 11:49:43 +0800

    Cleanup of BossRatScript.cpp

--- Purring_Engine/src/Logic/Boss/BossRatScript.cpp
+++ Purring_Engine/src/Logic/Boss/BossRatScript.cpp
@@ -223,6 +223,7 @@ namespace PE
 		}
 		return ClosestCat;
 	}
+
 	std::vector<EntityID> BossRatScript::GetAllObstacles()
 	{
 		return m_obstacles;
@@ -276,6 +277,7 @@ namespace PE
 			}
 		}
 	}
+
 	bool BossRatScript::IsObstacle(EntityID id)
 	{
 	 return (EntityManager::GetInstance().Get<EntityDescriptor>(id).name.find("Obstacle") != std::string::npos);;
@@ -328,6 +330,7 @@ namespace PE
 		
 
 	}
+
 	void BossRatScript::PlayAttackAudio()
 	{
 		int random = rand() % 5 + 1;
@@ -335,26 +338,30 @@ namespace PE
 		GlobalMusicManager::GetInstance().PlaySFX(attackAudio, false);
 
 	}
+
 	void BossRatScript::PlayHurtAudio()
 	{
 		int random = rand() % 8 + 1;
 		std::string hurtAudio = "AudioObject/BossAudioPrefab/BossRatInjured" + std::to_string(random) + "SFX.prefab";
 		GlobalMusicManager::GetInstance().PlaySFX(hurtAudio, false);
 	}
+
 	void BossRatScript::PlayDeathAudio()
 	{
 		//GlobalMusicManager::GetInstance().PlaySFX("AudioObject/BossAudioPrefab/BossRatDeath1SFX.prefab", false);
 		GlobalMusicManager::GetInstance().StartFadeOut("AudioObject/BossAudioPrefab/BossRatDeath1SFX.prefab", 3.0f);
-
 	}
+
 	void BossRatScript::PlayPoisonPuddleAudio()
 	{
 		GlobalMusicManager::GetInstance().PlaySFX("AudioObject/BossAudioPrefab/BossRatChargePuddlesSFX.prefab", false);
 	}
+
 	void BossRatScript::PlaySlamShockWaveAudio()
 	{
 		GlobalMusicManager::GetInstance().PlaySFX("AudioObject/BossAudioPrefab/BossRatSlamShockwaveSFX.prefab",false);
 	}
+
 	void BossRatScript::PlayBashSpikeAudio()
 	{
 		GlobalMusicManager::GetInstance().PlaySFX("AudioObject/BossAudioPrefab/BossRatBashSpikeSFX.prefab", false);
